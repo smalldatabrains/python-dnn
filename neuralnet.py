0c4f91a65c21120c97379243a356d684a93d5351
npignol <ho.npignol@cma-cgm.com>
2024-06-20 10:54:04 +0200

    adding bias gradient descent

--- neuralnet.py
+++ neuralnet.py
@@ -1,161 +1,152 @@
-#dependencies---------------------------------------------------------------------------------------
-
-import numpy as np #matrix multiplication + maths
-import pandas as pd#data management
+import numpy as np
+import pandas as pd
 import os
 import matplotlib.pyplot as plt
 
-#network definitions--------------------------------------------------------------------------------
-# Z : output vector for each layer
-# X : input vector for each layer
-# W : Weights matrix
-# a : values vector after applying the sigmoid function to Z, a becomes the new input vector for the next layer
-# Yreal : real value vector of the ouput
-# Yth : theoritical value vector calculated by our network after forward propagation
-# J : costfunction, return single value
-# gradient : gradient of J regarding the Weights
-
+# Activation functions and their derivatives
 def normalize(X):
-	return X/np.amax(X)
+    return X / np.amax(X)
 
 def sigmoid(z):
-	return 1.0/(1.0+np.exp(-z))
+    return 1.0 / (1.0 + np.exp(-z))
 
 def tanh(z):
-	return  np.tanh(z)
+    return np.tanh(z)
 
 def softmax(z):
-	softmax = np.exp(z-np.max(z))/np.sum(np.exp(z-np.max(z)),axis=1,keepdims=True)
-	return softmax
-		
+    return np.exp(z - np.max(z)) / np.sum(np.exp(z - np.max(z)), axis=1, keepdims=True)
+
 def dsigmoid(z):
-	return np.exp(-z)/(1+np.exp(-z))**2
+    return np.exp(-z) / (1 + np.exp(-z)) ** 2
 
 def dtanh(z):
-	return 1 - tanh(z)**2
+    return 1 - tanh(z) ** 2
 
-def Weights(k,l):
-	return np.random.randn(k,l)
+# Functions to initialize weights and biases
+def Weights(k, l):
+    return np.random.randn(k, l)
 
 def bias(l):
-	return np.random.rand(l) #it is better to start with positive values for bias
+    return np.random.rand(l)
 
-def create_network(input,n_hidden,n_neurons,output):
-	W=dict()
-	b=dict()
-	for i in range(n_hidden+1):
-		if i==0:
-			W[i]=Weights(input.shape[1],n_neurons[i])
-			b[i]=bias(n_neurons[i])
-		elif i==n_hidden:
-			W[i]=Weights(n_neurons[i-1],2)
-			b[i]=bias(2)
-		else:
-			W[i]=Weights(n_neurons[i-1],n_neurons[i])
-			b[i]=bias(n_neurons[i])
-	return W,b
+# Function to create the neural network
+def create_network(input, n_hidden, n_neurons, output):
+    W = dict()
+    b = dict()
+    for i in range(n_hidden + 1):
+        if i == 0:
+            W[i] = Weights(input.shape[1], n_neurons[i])
+            b[i] = bias(n_neurons[i])
+        elif i == n_hidden:
+            W[i] = Weights(n_neurons[i - 1], 2)
+            b[i] = bias(2)
+        else:
+            W[i] = Weights(n_neurons[i - 1], n_neurons[i])
+            b[i] = bias(n_neurons[i])
+    return W, b
 
-def forward(input,W,b):
-	for layer in range(len(W)):
-		if layer==0:
-			Yth=np.dot(input,W[layer])+b[layer]
-			Yth=sigmoid(Yth)
-		elif layer==len(W)-1:
-			Yth=np.dot(Yth,W[layer])+b[layer]
-			Yth=softmax(Yth)
-		else:
-			Yth=np.dot(Yth,W[layer])+b[layer]
-			Yth=sigmoid(Yth)
-	return Yth
+# Forward propagation function
+def forward(input, W, b):
+    for layer in range(len(W)):
+        if layer == 0:
+            Yth = np.dot(input, W[layer]) + b[layer]
+            Yth = sigmoid(Yth)
+        elif layer == len(W) - 1:
+            Yth = np.dot(Yth, W[layer]) + b[layer]
+            Yth = softmax(Yth)
+        else:
+            Yth = np.dot(Yth, W[layer]) + b[layer]
+            Yth = sigmoid(Yth)
+    return Yth
 
-def cost(Yth,Yreal):
-	J=np.sum((Yth-Yreal)**2)/(2*len(Yreal))
-	return J
+# Cost function
+def cost(Yth, Yreal):
+    J = np.sum((Yth - Yreal) ** 2) / (2 * len(Yreal))
+    return J
 
-def gradient(Yth,Yreal,X,J,W):
-	gradient=dict()
-	delta=dict()
-	Z=dict()
-	a=dict()
-	for layer in range(len(W)):
-		if layer==0:
-			Z[layer]=np.dot(X,W[layer])
-			a[layer]=sigmoid(Z[layer])
-		else:
-			Z[layer]=np.dot(a[layer-1],W[layer])
-			a[layer]=sigmoid(Z[layer])
+# Gradient computation function
+def gradient(Yth, Yreal, X, W):
+    gradient_W = dict()
+    gradient_b = dict()
+    delta = dict()
+    Z = dict()
+    a = dict()
 
+    for layer in range(len(W)):
+        if layer == 0:
+            Z[layer] = np.dot(X, W[layer]) + b[layer]
+            a[layer] = sigmoid(Z[layer])
+        else:
+            Z[layer] = np.dot(a[layer - 1], W[layer]) + b[layer]
+            a[layer] = sigmoid(Z[layer])
 
-	for layer in reversed(range(len(W))):
-		if layer==len(W)-1:
-			delta[layer]=np.multiply((Yth-Yreal),dsigmoid(Z[layer]))
-			gradient[layer]=np.dot(a[layer-1].transpose(),delta[layer])
+    for layer in reversed(range(len(W))):
+        if layer == len(W) - 1:
+            delta[layer] = np.multiply((Yth - Yreal), dsigmoid(Z[layer]))
+            gradient_W[layer] = np.dot(a[layer - 1].transpose(), delta[layer])
+            gradient_b[layer] = np.sum(delta[layer], axis=0, keepdims=True)
+        else:
+            if layer == 0:
+                delta[layer] = np.dot(delta[layer + 1], W[layer + 1].transpose()) * dsigmoid(Z[layer])
+                gradient_W[layer] = np.dot(X.transpose(), delta[layer])
+                gradient_b[layer] = np.sum(delta[layer], axis=0, keepdims=True)
+            else:
+                delta[layer] = np.dot(delta[layer + 1], W[layer + 1].transpose()) * dsigmoid(Z[layer])
+                gradient_W[layer] = np.dot(a[layer - 1].transpose(), delta[layer])
+                gradient_b[layer] = np.sum(delta[layer], axis=0, keepdims=True)
 
-		else:
-			if layer==0:
-				delta[layer]=np.dot(delta[layer+1],W[layer+1].transpose())*dsigmoid(Z[layer])
-				gradient[layer]=np.dot(X.transpose(),delta[layer])
-			else:
-				delta[layer]=np.dot(delta[layer+1],W[layer+1].transpose())*dsigmoid(Z[layer])
-				gradient[layer]=np.dot(a[layer-1].transpose(),delta[layer])
+    return gradient_W, gradient_b, a
 
+# Gradient descent function
+def gradient_descent(gradient_W, gradient_b, W, b, learning_rate):
+    for layer in range(len(W)):
+        W[layer] = W[layer] - learning_rate * gradient_W[layer]
+        b[layer] = b[layer] - learning_rate * gradient_b[layer].squeeze()
+    return W, b
 
-	return gradient,a
+# Data retrieving
+data = pd.read_csv("data.csv", sep=";")
 
-def gradient_descent(gradient,W,b,learning_rate):
-	for layer in range(len(W)):
-		W[layer]=W[layer]-learning_rate*gradient[layer]
-	return W
+# m: number of examples, p: number of features
+m = data.shape[0]
+p = data.shape[1]
 
-#data retrieving------------------------------------------------------------------------------------
-os.chdir("")
-data=pd.read_csv("data.csv",sep=";")
-
-#m : number of exemples, p : numbers of features
-m=data.shape[0]
-p=data.shape[1]
-
-#Separating Inputs and outputs ---------------------------------------------------------------------
-X=data.ix[:,0:p-1] #not including p-1
-y=data.iloc[:,p-1]
-Yreal=[]
+# Separating inputs and outputs
+X = data.iloc[:, 0:p-1]
+y = data.iloc[:, p-1]
+Yreal = np.zeros((len(y), 2))
 for i in range(len(y)):
-	if y[i]==1:
-		Yreal.append([1,0])
-	elif y[i]==0:
-		Yreal.append([0,1])
+    if y[i] == 1:
+        Yreal[i] = [1, 0]
+    elif y[i] == 0:
+        Yreal[i] = [0, 1]
 
-#parameters-----------------------------------------------------------------------------------------
-n_neurons=[4]
-n_hidden=len(n_neurons)
+# Parameters
+n_neurons = [4]
+n_hidden = len(n_neurons)
 
-#normalization of the inputs and network initialization---------------------------------------------
-X=normalize(X)
-W,b=create_network(X,n_hidden,n_neurons,y)
-	
-#Calculate 1st forward propagation and initial cost-------------------------------------------------
-Yth=forward(X,W,b)
+# Normalization of the inputs and network initialization
+X = normalize(X)
+W, b = create_network(X, n_hidden, n_neurons, y)
 
-J=cost(Yth,Yreal)
+# Calculate 1st forward propagation and initial cost
+Yth = forward(X, W, b)
+J = cost(Yth, Yreal)
+learning_rate = 0.0005
 
-learning_rate=0.0005
+# Training the network
+for epoch in range(3000):
+    Yth = forward(X, W, b)
+    J = cost(Yth, Yreal)
+    print('Cost is:', J)
+    gradient_W, gradient_b, a = gradient(Yth, Yreal, X, W)
+    W, b = gradient_descent(gradient_W, gradient_b, W, b, learning_rate)
 
-#training the network-------------------------------------------------------------------------------
-for epoch in range (3000):
-	Yth=forward(X,W,b)
-	J=cost(Yth,Yreal)
-	print('cost is : ',J)
-	grad,a=gradient(Yth,Yreal,X,J,W)
-	for layer in range(len(W)):
-		W[layer]=gradient_descent(grad[layer],W[layer],b[layer],learning_rate)
-
-	
-inter=np.reshape(a[0][1],[2,2])
-plt.matshow(inter)	
+# Plotting the activation of the first hidden layer neurons
+inter = np.reshape(a[0][1], [2, 2])
+plt.matshow(inter)
 plt.show()
-#prediction on test set-----------------------------------------------------------------------------
-Yth=forward(X,W,b)
+
+# Prediction on the test set
+Yth = forward(X, W, b)
 print(Yth)
-
-#accuracy measurement-------------------------------------------------------------------------------
-
